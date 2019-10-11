8ce0d671e91a8c268eb4b40607c11f692a752a91
Brian De Sousa <briandesousa1@gmail.com>
2019-10-10 22:07:17 -0400

    add constructors for weather data classes

--- lib/weather-data.dart
+++ lib/weather-data.dart
@@ -2,11 +2,13 @@ class WeatherData {
   String location;
   String temperature;
   String weatherDescription;
+
+  WeatherData([this.location, this.temperature, this.weatherDescription]);
 }
 
 class Location {
   String name;
   String code;
 
-  Location(this.name, this.code);
+  Location([this.name, this.code]);
 }
